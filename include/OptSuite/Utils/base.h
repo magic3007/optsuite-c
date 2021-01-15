8edc8288b098af982f1900aad76613a3f30de5cf
magic3007 <maijing3007@qq.com>
2021-01-15 22:56:07 +0800

    [utils] commit "stopwatch".

---
+++ include/OptSuite/Utils/base.h
@@ -0,0 +1,37 @@
+/**
+ * File              : base.h
+ * Author            : Jing Mai <jingmai@pku.edu.cn>
+ * Date              : 01.15.2021
+ * Last Modified Date: 01.15.2021
+ * Last Modified By  : Jing Mai <jingmai@pku.edu.cn>
+ */
+#pragma once
+
+#include <functional>
+// uncomment to disable assert()
+// #define NDEBUG
+#include <cassert>
+
+// Use (void) to silent unused warnings.
+#define assertm(exp, msg) assert(((void) msg, exp))
+
+#define DISALLOW_COPY_AND_ASSIGN(ClassName)                                                        \
+private:                                                                                           \
+    ClassName(const ClassName &) = delete;                                                         \
+    void operator=(const ClassName &) = delete
+
+#define DEFER_CONCAT_IMPL(x, y) x##y
+#define DEFER_CONCAT(x, y)      DEFER_CONCAT_IMPL(x, y)
+
+class DeferredAction {
+    DISALLOW_COPY_AND_ASSIGN(DeferredAction);
+
+public:
+    explicit DeferredAction(std::function<void(void)> f) : f_(std::move(f)) {}
+    ~DeferredAction() { f_(); }
+
+private:
+    std::function<void(void)> f_;
+};
+
+#define DEFER(action) DeferredAction DEFER_CONCAT(__deferred__, __LINE__)([&]() -> void action)
