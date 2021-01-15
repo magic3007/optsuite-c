dd630614d736f3504f1ae0e7033dda65eb7d3a0c
magic3007 <maijing3007@qq.com>
2021-01-15 22:57:34 +0800

    [solver] commit proximal gradient method

--- include/OptSuite/Base/solver.h
+++ include/OptSuite/Base/solver.h
@@ -3,7 +3,7 @@
  *
  *       Filename:  solver.h
  *
- *    Description:  
+ *    Description:
  *
  *        Version:  1.0
  *        Created:  11/05/2020 03:39:06 PM
@@ -19,45 +19,70 @@
 #ifndef OPTSUITE_BASE_SOLVER_H
 #define OPTSUITE_BASE_SOLVER_H
 
-#include <string>
+#include "OptSuite/Base/functional.h"
 #include "OptSuite/core_n.h"
+#include <functional>
+#include <string>
 
-namespace OptSuite { namespace Base {
-    class SolverBase {
-        public:
-            SolverBase(std::string name_) : name(name_) {}
-            ~SolverBase() = default;
+namespace OptSuite {
+namespace Base {
+class SolverBase {
+public:
+    SolverBase(std::string name_) : name(name_) {}
+    ~SolverBase() = default;
 
-            inline
-            virtual void read_options_from_file(std::string) {}
+    inline virtual void read_options_from_file(std::string) {}
 
-            std::string name;
-    };
+    std::string name;
+};
 
-    class SolverOptions {
-        public:
-            // getter
-            Scalar ftol();
-            Scalar gtol();
-            Scalar xtol();
-            Index maxit();
-            Verbosity verbosity();
+class SolverOptions {
+    using StopConditionChecker = typename std::function<bool(const std::vector<Scalar> &)>;
 
-            // setter
-            void ftol(Scalar);
-            void gtol(Scalar);
-            void xtol(Scalar);
-            void maxit(Index);
-            void verbosity(Verbosity);
+public:
+    // getter
+    Scalar                      ftol();
+    Scalar                      gtol();
+    Scalar                      xtol();
+    Index                       maxit();
+    Verbosity                   verbosity();
+    const StopConditionChecker &stop_condition_checker() { return checker_; }
+    // setter
+    void ftol(Scalar);
+    void gtol(Scalar);
+    void xtol(Scalar);
+    void maxit(Index);
+    void verbosity(Verbosity);
+    void stop_condition_checker(StopConditionChecker checker) { checker_ = std::move(checker); }
 
-        protected:
-            Scalar ftol_;
-            Scalar gtol_;
-            Scalar xtol_;
+protected:
+    Scalar ftol_;   ///< The objective value variation tolerance
+    Scalar gtol_;   ///< The gradient norm tolerance
+    Scalar xtol_;   ///< The independent variable tolerance
 
-            Index maxit_;
-            Verbosity verbosity_;
-    };
-}}
+    Index                maxit_;
+    Verbosity            verbosity_;
+    StopConditionChecker checker_;
+};
+
+struct SolverRecords {
+    Index               n_iters;
+    std::vector<Scalar> f_hist;
+    time_t              elapsed_time_us;
+};
+
+class ProximalGradSolver : public SolverBase {
+public:
+    ProximalGradSolver(std::string name, SolverOptions options)
+        : SolverBase(name), options_(options) {}
+    void operator()(Ref<const Mat> x0, FuncGrad<Scalar> &func_f, Func<Scalar> func_h,
+                    Proximal<Scalar> &h_prox, Scalar t, Ref<Mat> result, SolverRecords &records);
+
+protected:
+    SolverOptions options_;
+};
+}   // namespace Base
+}   // namespace OptSuite
+
 
 #endif
--- src/Base/solver.cpp
+++ src/Base/solver.cpp
@@ -3,7 +3,7 @@
  *
  *       Filename:  solver.cpp
  *
- *    Description:  
+ *    Description:
  *
  *        Version:  1.0
  *        Created:  11/06/2020 06:46:26 PM
@@ -16,36 +16,61 @@
  * ==========================================================================
  */
 
-#include "OptSuite/core_n.h"
 #include "OptSuite/Base/solver.h"
+#include "OptSuite/Utils/stopwatch.hpp"
+#include "OptSuite/core_n.h"
 
-namespace OptSuite { namespace Base {
-    Scalar SolverOptions::ftol() { return ftol_; }
-    Scalar SolverOptions::gtol() { return gtol_; }
-    Scalar SolverOptions::xtol() { return xtol_; }
+namespace OptSuite {
+namespace Base {
+Scalar SolverOptions::ftol() { return ftol_; }
+Scalar SolverOptions::gtol() { return gtol_; }
+Scalar SolverOptions::xtol() { return xtol_; }
 
-    Index SolverOptions::maxit() { return maxit_; }
-    Verbosity SolverOptions::verbosity() { return verbosity_; }
+Index     SolverOptions::maxit() { return maxit_; }
+Verbosity SolverOptions::verbosity() { return verbosity_; }
 
-    void SolverOptions::ftol(Scalar v){
-        OPTSUITE_ASSERT(v >= 0);
-        ftol_ = v;
-    }
-    void SolverOptions::gtol(Scalar v){
-        OPTSUITE_ASSERT(v >= 0);
-        gtol_ = v;
-    }
-    void SolverOptions::xtol(Scalar v){
-        OPTSUITE_ASSERT(v >= 0);
-        xtol_ = v;
-    }
+void SolverOptions::ftol(Scalar v) {
+    OPTSUITE_ASSERT(v >= 0);
+    ftol_ = v;
+}
+void SolverOptions::gtol(Scalar v) {
+    OPTSUITE_ASSERT(v >= 0);
+    gtol_ = v;
+}
+void SolverOptions::xtol(Scalar v) {
+    OPTSUITE_ASSERT(v >= 0);
+    xtol_ = v;
+}
 
-    void SolverOptions::maxit(Index v){
-        OPTSUITE_ASSERT(v > 0);
-        maxit_ = v;
-    }
-    void SolverOptions::verbosity(Verbosity v){
-        verbosity_ = v;
-    }
-}}
+void SolverOptions::maxit(Index v) {
+    OPTSUITE_ASSERT(v > 0);
+    maxit_ = v;
+}
+void SolverOptions::verbosity(Verbosity v) { verbosity_ = v; }
 
+void ProximalGradSolver::operator()(Ref<const Mat> x0, FuncGrad<Scalar> &func_f,
+                                    Func<Scalar> func_h, Proximal<Scalar> &h_prox, Scalar t,
+                                    Ref<Mat> result, SolverRecords &records) {
+    stopwatch::Stopwatch stopwatch;
+    stopwatch.start();
+    Mat                 x = x0;
+    Mat                 grad_f;
+    Index               i;
+    std::vector<Scalar> f_hist;
+    for (i = 0; i < options_.maxit(); i++) {
+        Scalar f_val   = func_f(x, grad_f, true);
+        Scalar h_val   = func_h(x);
+        Scalar obj_val = f_val + t * h_val;
+        f_hist.push_back(obj_val);
+        if (options_.stop_condition_checker()(f_hist)) { break; }
+        // TODO(Jing Mai, jingmai@pku.edu.cn): add step size strategies.
+        Scalar step_size = 1;
+        h_prox(x.array() - step_size * grad_f.array(), /* t */ t, x);
+    }
+    result                  = x;
+    records.elapsed_time_us = stopwatch.elapsed<stopwatch::mus>();
+    records.n_iters         = i;
+    records.f_hist          = std::move(f_hist);
+}
+}   // namespace Base
+}   // namespace OptSuite
