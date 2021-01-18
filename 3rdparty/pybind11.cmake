7c96cc6860013ced8eb3ef38351d08d4e4f66acb
magic3007 <maijing3007@qq.com>
2021-01-18 11:40:22 +0800

    [CMake] commit "pybind11"

---
+++ 3rdparty/pybind11.cmake
@@ -0,0 +1,21 @@
+configure_file(pybind11.cmake.in pybind11-download/CMakeLists.txt)
+
+message(STATUS "Downloading pybind11")
+execute_process(COMMAND ${CMAKE_COMMAND} -G "${CMAKE_GENERATOR}" .
+        RESULT_VARIABLE result
+        WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/pybind11-download)
+if (result)
+    message(FATAL_ERROR "CMake step for pybind11 failed: ${result}")
+endif ()
+execute_process(COMMAND "${CMAKE_COMMAND}" --build .
+        RESULT_VARIABLE result
+        WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/pybind11-download)
+if (result)
+    message(FATAL_ERROR "Build step for pybind11 failed: ${result}")
+endif ()
+
+
+add_subdirectory(${CMAKE_CURRENT_BINARY_DIR}/pybind11-src
+        ${CMAKE_CURRENT_BINARY_DIR}/pybind11-build
+        EXCLUDE_FROM_ALL)
+
